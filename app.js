dac5902cf5bdd371538f77030470d15658839d6f
Asif-patel <asifp6021@gmail.com>
2023-10-22 11:56:36 +0530

    3 -> created dynamic element to show toDo dynamically on webpage and created empty array to store toDo in it.

--- app.js
+++ app.js
@@ -10,8 +10,21 @@ const checkBtn = 'fa-check-circle';
 const uncheckBtn = 'fa-circle-thin';
 const textLineThrough = 'line-through';
 
+// created array container for toDo
+let toDoConatainer = [];
+let id = 0;
+
 //addToDo function
 function addToDo(toDo, id, done, trace) {
 	const toDoDone = done ? checkBtn : uncheckBtn;
 	const toDoLine = done ? textLineThrough : '';
+
+	// dynamic elemenents
+	const item = `
+                    <li class="item">
+                        <i class="fa ${toDoDone} complete" status="compelte" id="${id}"></i>
+                        <p class="text ${toDoLine}">${toDo}</p>
+                        <i class="fa fa-trash-o delete" status="delete" id="${id}"></i>
+                    </li>
+                `;
 }
