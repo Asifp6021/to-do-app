4a34cbfe7ec13feac12314e5c593b5b129d15963
Asif-patel <asifp6021@gmail.com>
2023-10-22 12:28:25 +0530

    4 -> inserted created dynamic elements on webpage and did test of toDoAdd() function.

--- app.js
+++ app.js
@@ -3,19 +3,25 @@ const toDoList = document.querySelector('#list');
 const toDoInput = document.querySelector('#input');
 const ToDoAddBtn = document.querySelector('.fa-plus-circle');
 
-//- - -- - - -- - - - -- - -- -- - - -- - - - --- - - - -- - - -- - - - - -- -
+//- - -- - - -- - - - -- - -- -- - - -- - - - --- - - - -- - - -- - - - - -- - -  - - - - - - -
 
 // storing dynamic icon classes in variable.
 const checkBtn = 'fa-check-circle';
 const uncheckBtn = 'fa-circle-thin';
 const textLineThrough = 'line-through';
 
+//- - -- - - -- - - - -- - -- -- - - -- - - - --- - - - -- - - -- - - - - -- - -  - - - - - - -
+
 // created array container for toDo
 let toDoConatainer = [];
 let id = 0;
 
+//- - -- - - -- - - - -- - -- -- - - -- - - - --- - - - -- - - -- - - - - -- - -  - - - - - - -
+
 //addToDo function
-function addToDo(toDo, id, done, trace) {
+function addToDo(toDo, id, done, trash) {
+	if (trash) return;
+
 	const toDoDone = done ? checkBtn : uncheckBtn;
 	const toDoLine = done ? textLineThrough : '';
 
@@ -27,4 +33,16 @@ function addToDo(toDo, id, done, trace) {
                         <i class="fa fa-trash-o delete" status="delete" id="${id}"></i>
                     </li>
                 `;
-}
+
+	// https://developer.mozilla.org/en-US/docs/Web/API/Element/insertAdjacentHTML
+	//  inserting created dynamic element
+	const toDoItemPosition = 'beforeend';
+	toDoList.insertAdjacentHTML(toDoItemPosition, item);
+};
+
+// i have't created eventlistener so i am testing this function by passing parameter
+// addToDo('eating breakfast', 0, false, false);
+//  addToDo('eating breakfast', 0, true, false);
+//  addToDo('eating breakfast', 0, true, true);
+
+//- - -- - - -- - - - -- - -- -- - - -- - - - --- - - - -- - - -- - - - - -- - -  - - - - - - -
